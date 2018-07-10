3063b595506fca3595197912934b1044744ece52
Ryan M Taylor <mailryan@gmail.com>
2018-07-10 15:13:11 -0400

    added ecmascript

--- package.js
+++ package.js
@@ -2,10 +2,11 @@ Package.describe({
   name: 'gwendall:accounts-helpers',
   summary: 'Useful helpers for Accounts',
   version: '0.1.7',
-  git: 'https://github.com/gwendall/meteor-accounts-helpers'
+  git: 'https://github.com/gwendall/meteor-accounts-helpers',
 });
 
 var packages = [
+  'ecmascript@0.1.5',
   'check@1.0.6',
   'accounts-base@1.2.1',
   'accounts-ui@1.1.6',
@@ -13,21 +14,19 @@ var packages = [
   'templating@1.1.4',
   'service-configuration@1.0.5',
   'gwendall:body-events@0.1.6',
-  'aldeed:simple-schema@1.3.3'
+  'aldeed:simple-schema@1.3.3',
 ];
 
 Package.on_use(function(api, where) {
-
   api.use(packages);
   api.imply(packages);
 
   api.add_files([
     'client/hooks.js',
-    'client/helpers.js'
+    'client/helpers.js',
   ], 'client');
 
   api.add_files([
-    'server/lib.js'
+    'server/lib.js',
   ], 'server');
-
 });
